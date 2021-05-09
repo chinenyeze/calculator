1266bb3dd8c7862d2abad8f45002367b3a89351b
Samuel Chinenyeze <samuel.chinenyeze@survey54.com>
2021-05-09 15:15:53 +0100

    Add validator

--- src/Framework/Validator/Calculator/PostCalculateValidator.php
+++ src/Framework/Validator/Calculator/PostCalculateValidator.php
@@ -4,15 +4,27 @@ namespace Company\Calculator\Framework\Validator\Calculator;
 
 use Company\Calculator\Framework\Validator\ValidatorInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Respect\Validation\Exceptions\ValidationException;
+use Respect\Validation\Validator as v;
 
 class PostCalculateValidator implements ValidatorInterface
 {
     /**
      * @param ServerRequestInterface $request
      * @return array
+     * @throws ValidationException
      */
     public function validate(ServerRequestInterface $request): array
     {
-        // TODO: Implement validate() method.
+        $params = $request->getParsedBody();
+
+        v::arrayType()
+            ->key('left', v::numericVal())
+            ->key('right', v::numericVal())
+            ->key('operator', v::stringVal())
+            ->setName('payload')
+            ->check($params);
+
+        return $params;
     }
 }
--- src/Framework/Validator/ValidatorInterface.php
+++ src/Framework/Validator/ValidatorInterface.php
@@ -3,12 +3,14 @@
 namespace Company\Calculator\Framework\Validator;
 
 use Psr\Http\Message\ServerRequestInterface;
+use Respect\Validation\Exceptions\ValidationException;
 
 interface ValidatorInterface
 {
     /**
      * @param ServerRequestInterface $request
      * @return array
+     * @throws ValidationException
      */
     public function validate(ServerRequestInterface $request): array;
 }
