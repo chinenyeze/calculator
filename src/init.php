63f8269bff28d7dfcb93d3b66cfd0ef9d039a1de
Samuel Chinenyeze <samuel.chinenyeze@survey54.com>
2021-05-09 13:47:43 +0100

    Add service providers and bootstrap files

---
+++ src/init.php
@@ -0,0 +1,20 @@
+<?php
+
+use Company\Calculator\Framework\ServiceProvider\ApplicationServiceProvider;
+use Company\Calculator\Framework\ServiceProvider\ControllerServiceProvider;
+use Company\Calculator\Framework\ServiceProvider\RouteServiceProvider;
+use DI\ContainerBuilder;
+use Slim\Factory\AppFactory;
+
+$container = (new ContainerBuilder())
+    ->useAutowiring(false)
+    ->useAnnotations(false)
+    ->build();
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+
+(new ApplicationServiceProvider())->register($container);
+(new ControllerServiceProvider())->register($container);
+(new RouteServiceProvider())->registerRoutes($app);
+
+$app->run();
