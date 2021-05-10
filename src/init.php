be70f67ab544a47ba3e7ec6fb8fdcbfa0b98f8de
Samuel Chinenyeze <samuel.chinenyeze@survey54.com>
2021-05-10 03:39:52 +0100

    Add twig middleware

--- src/init.php
+++ src/init.php
@@ -2,6 +2,7 @@
 
 use Company\Calculator\Framework\ServiceProvider\ApplicationServiceProvider;
 use Company\Calculator\Framework\ServiceProvider\ControllerServiceProvider;
+use Company\Calculator\Framework\ServiceProvider\MiddlewareServiceProvider;
 use Company\Calculator\Framework\ServiceProvider\RouteServiceProvider;
 use DI\ContainerBuilder;
 use Slim\Factory\AppFactory;
@@ -15,6 +16,7 @@ $app = AppFactory::create();
 
 (new ApplicationServiceProvider())->register($container);
 (new ControllerServiceProvider())->register($container);
+(new MiddlewareServiceProvider())->register($container);
 (new RouteServiceProvider())->registerRoutes($app);
 
 $app->run();
